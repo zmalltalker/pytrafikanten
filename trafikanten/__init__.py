03bb007e96f4d91a47fea895ebe10001d0da0712
Marius Mathiesen <marius.mathiesen@gmail.com>
2009-03-18 12:09:52 +0100

    Merging runes changes from bitbucket

--- trafikanten/__init__.py
+++ trafikanten/__init__.py
@@ -1,20 +1,21 @@
 # coding=utf-8
 """
-    Trafikanten
-    ~~~~~~~~~~~
+Trafikanten
+~~~~~~~~~~~
 
-    Trafikanten is a library for accessing realtime data about departures
-    fromthe Norwegian public transportation system. It covers mainly Oslo
-    and Akershus counties.
+Trafikanten is a library for accessing realtime data about departures
+from the Norwegian public transportation system. It covers mainly Oslo
+and Akershus counties.
 
-    :copyright: Copyright 2009. see AUTHORS.
-    :license: BSD, see LICENSE for details.
+:copyright: Copyright 2009 by the authors. see AUTHORS.
+:license: BSD, see LICENSE for details.
 """
-from .api import find_station, get_realtime
-from .classes import CachingTrafikanten, FileCacheTrafikanten, MemoryCacheTrafikanten
 
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Rune Halvorsen <runefh@gmail.com>'
 __url__ = 'http://bitbucket.org/runeh/pytrafikanten/'
 __license__ = 'BSD License'
 __docformat__ = 'restructuredtext'
+
+from .api import find_station, get_realtime
+from .classes import CachingTrafikanten, FileCacheTrafikanten, MemoryCacheTrafikanten
